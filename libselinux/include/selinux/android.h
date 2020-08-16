cf8d6c4b1f95f7fa75e17e333f14930afc3e7ae9
Surge Raval <Surge1223@gmail.com>
2020-08-16 08:59:59 -0400

    2

--- libselinux/include/selinux/android.h
+++ libselinux/include/selinux/android.h
@@ -13,13 +13,13 @@ extern "C" {
 
 extern struct selabel_handle* selinux_android_file_context_handle(void);
 
-extern struct selabel_handle* selinux_android_prop_context_handle(void);
-
 extern struct selabel_handle* selinux_android_service_context_handle(void);
 
 extern struct selabel_handle* selinux_android_hw_service_context_handle(void);
 
 extern struct selabel_handle* selinux_android_vendor_service_context_handle(void);
+
+extern struct selabel_handle* selinux_android_keystore2_key_context_handle(void);
 
 extern void selinux_android_set_sehandle(const struct selabel_handle *hndl);
 
@@ -34,10 +34,10 @@ extern int selinux_android_setcontext(uid_t uid,
 				      const char *seinfo,
 				      const char *name);
 
-extern int selinux_android_setfilecon(const char *pkgdir,
-				       const char *pkgname,
-				       const char *seinfo,
-				       uid_t uid);
+extern int selinux_android_context_with_level(const char * context,
+					      char ** newContext,
+					      uid_t userid,
+					      uid_t appid);
 
 extern int selinux_log_callback(int type, const char *fmt, ...)
     __attribute__ ((format(printf, 2, 3)));
